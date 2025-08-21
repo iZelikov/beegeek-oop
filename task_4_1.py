468866c7537d2e02bda0216df4ac9317eedd6ca8
iZelikov <ivan@zelikov.name>
2025-08-22 01:05:16 +0300

    finish 4.1

---
+++ task_4_1.py
@@ -0,0 +1,11 @@
+class PiggyBank:
+    content = 'coins'
+    alternate_name = 'penny bank'
+
+money_box = PiggyBank()
+
+money_box1 = PiggyBank()
+money_box1.coins = 10
+money_box2 = PiggyBank()
+money_box2.coins = 15
+money_box2.color = 'pink'
